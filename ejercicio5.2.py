9e19d7a56cb7ce4b13b3297f274c041b81bac23a
Facundo20030 <facundoprado17@gmail.com>
2025-04-03 19:27:56 -0300

    Add files via upload

---
+++ ejercicio5.2.py
@@ -0,0 +1,23 @@
+
+fruta = ["manzana", "pera", "uva", "mandarina"]
+fruta.append("banana")
+fruta.insert(2, "durazno")
+fruta.remove("uva")
+fruta.sort()
+fruta.reverse()
+for frutas in fruta:
+    print(frutas)
+    
+
+#while #repite un bloque mientras una condicion sea verdadera
+# contador = 3 //asigno un valor a la variable
+# contador = 5 // hacer comparacion
+
+contador=0
+while True:
+    print(contador)
+    contador +=1
+    if contador == 5:
+        break
+    
+    
